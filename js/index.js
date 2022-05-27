ef1f95feee00fe55053752dd70bbb3a5f1e45996
Koshyar-r <belfi7777@gmail.com>
2022-05-27 05:21:47 -0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,32 @@
+const Body = document.querySelector("body")
+const NavMenu = Body.querySelector(".menu__content")
+const NavOpenBtn = Body.querySelector(".NavOpen")
+const NavCloseBtn = NavMenu.querySelector(".NavClose")
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+if(NavMenu && NavOpenBtn) {
+    NavOpenBtn.addEventListener("click", () => {
+        NavMenu.classList.add("open")
+        body.style.overflowY = "hidden"
+    })
+}
+
+if(NavMenu && NavCloseBtn) {
+    NavCloseBtn.addEventListener("click", () => {
+        NavMenu.classList.remove("open")
+        body.style.overflowY = "scroll"
+    })
+}
