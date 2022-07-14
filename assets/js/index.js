e8c4a1c1e95d72d954b043874fa4d577995975b4
Koshyar-r <belfi7777@gmail.com>
2022-07-14 05:44:20 -0700

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -49,19 +49,56 @@ function scrollActive(){
 window.addEventListener('scroll', scrollActive)
 
 
-let TopBtn = document.getElementById("TopBtn");
+let TopBtn = document.getElementById("TopBtn")
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()}
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    TopBtn.style.display = "block";
+    TopBtn.style.display = "block"
   } else {
-    TopBtn.style.display = "none";
+    TopBtn.style.display = "none"
   }
 }
 
 function TopFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
 }
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+const ScrollAnimation = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+ScrollAnimation.reveal(`.home-data`)
+ScrollAnimation.reveal(`.home-img`, {delay: 500})
+ScrollAnimation.reveal(`.about-img, .contact-box`, {origin: 'left'})
+ScrollAnimation.reveal(`.about-data, .contact-form`, {origin: 'right'})
+ScrollAnimation.reveal(`.product-card, .footer`, {interval: 100})
